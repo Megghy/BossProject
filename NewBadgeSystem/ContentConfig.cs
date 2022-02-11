ab53ed788b5291943af00ef43b7855e9cb850abc
Jeoican <42536930+Jeoican@users.noreply.github.com>
2022-02-11 14:14:54 +0800

    括[前缀][后缀]号 新头衔

---
+++ NewBadgeSystem/ContentConfig.cs
@@ -0,0 +1,41 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TShockAPI;
+
+namespace BadgeSystem
+{
+	internal sealed class ContentConfig
+	{
+		public List<Content> Content = new List<Content>();
+
+		public static readonly string FilePath = Path.Combine(TShock.SavePath, "newBadge.json");
+
+		public bool TryParse(string str, out Content b,string type)
+		{
+			b = Content.SingleOrDefault((Content x) => string.Equals(x.Identifier, str, StringComparison.Ordinal));
+			return b != null&&b.Type==type;
+		}
+
+		public static ContentConfig Read()
+		{
+			if (!File.Exists(FilePath))
+			{
+				return new ContentConfig();
+			}
+			using FileStream stream = new FileStream(FilePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+			using StreamReader streamReader = new StreamReader(stream);
+			return JsonConvert.DeserializeObject<ContentConfig>(streamReader.ReadToEnd());
+		}
+
+		public void Write()
+		{
+			using FileStream stream = new FileStream(FilePath, FileMode.Create, FileAccess.Write, FileShare.Write);
+			string value = JsonConvert.SerializeObject(this, Formatting.Indented);
+			using StreamWriter streamWriter = new StreamWriter(stream);
+			streamWriter.Write(value);
+		}
+	}
+}
